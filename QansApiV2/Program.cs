d1a23b963dffd010c554aac234cd0f1ff6543898
smrutiranjan-og <srpati25@gmmail.com>
2026-04-14 12:18:43 +0530

    env fixed

--- QansApiV2/Program.cs
+++ QansApiV2/Program.cs
@@ -150,7 +150,7 @@ var app = builder.Build();
 //app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
